a3dd12bc7e9ea580bb734f03a5ff94eeaa181b7e
Ivan Kaurov <crazyanimalcz@gamil.com>
2025-03-26 23:42:30 +0300

    Fourth homework

--- catalog/views.py
+++ catalog/views.py
@@ -8,46 +8,33 @@ from catalog.models import Product, Contact
 class IndexView(TemplateView):
     template_name = 'catalog/index.html'
     extra_context = {
+        'title': 'Главная'
+    }
+
+
+class CatalogListView(ListView):
+    model = Product
+    extra_context = {
         'title': 'Каталог'
     }
 
-    def get_context_data(self, **kwargs):
-        context_data = super().get_context_data(**kwargs)
-        context_data['object_list'] = Product.objects.all()
-        return context_data
 
-
-def contacts(request):
-    if request.method == 'POST':
-        name = request.POST.get('name')
-        phone = request.POST.get('phone')
-        message = request.POST.get('message')
-        print(name, phone, message)  # решил пока собранные данные выводить в консоль
-    context = {
+class ContactsView(TemplateView):
+    template_name = 'catalog/contacts.html'
+    extra_context = {
         'object_list': Contact.objects.all(),
         'title': 'Контакты'
     }
-    return render(request, 'catalog/contacts.html', context)
-
-
-class ContactsListView(ListView):
-    model = Contact
-
-
-def product(request, pk):
-    context = {
-        'object': Product.objects.get(pk=pk),
-        'title': 'Товар'
-    }
-    return render(request, 'catalog/product.html', context)
 
 
 class ProductDetailView(DetailView):
     model = Product
+    template_name = 'catalog/product.html'
 
 
-def basket(request):
-    context = {
+class BasketListView(ListView):
+    model = Product
+    extra_context = {
         'title': 'Корзина'
     }
-    return render(request, 'catalog/basket.html', context)
+    template_name = 'catalog/basket.html'
