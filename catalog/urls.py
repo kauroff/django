83bf909fbeae8083b061ef647afd44083a6f80ea
Ivan Kaurov <crazyanimalcz@gamil.com>
2025-03-26 12:28:16 +0300

    Fourth homework

--- catalog/urls.py
+++ catalog/urls.py
@@ -1,12 +1,12 @@
 from django.urls import path
 
 from catalog.apps import CatalogConfig
-from catalog.views import index, contacts, product, basket
+from catalog.views import contacts, product, basket, IndexView
 
 app_name = CatalogConfig.name
 
 urlpatterns = [
-    path('', index, name='index'),
+    path('', IndexView.as_view(), name='index'),
     path('contacts/', contacts, name='contacts'),
     path('<int:pk>/product/', product, name='product'),
     path('basket/', basket, name='basket'),
