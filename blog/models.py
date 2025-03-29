a7f2773cdb9703e737d242334680c23f9c551cd1
Ivan Kaurov <crazyanimalcz@gamil.com>
2025-03-29 16:51:51 +0300

    Fifth homework

--- blog/models.py
+++ blog/models.py
@@ -8,7 +8,7 @@ class Post(models.Model):
     slug = models.CharField(**NULLABLE, max_length=100, verbose_name='slug')
     body = models.TextField(**NULLABLE, verbose_name='содержимое')
     image = models.ImageField(upload_to='posts/', **NULLABLE, verbose_name='изображение')
-    create_date = models.DateTimeField(verbose_name='дата создания', auto_now_add=True)
+    create_date = models.DateField(verbose_name='дата создания', auto_now_add=True)
     is_published = models.BooleanField(verbose_name='опубликован', default=True)
     views_count = models.IntegerField(default=0, verbose_name='количество просмотров')
 
@@ -18,5 +18,3 @@ class Post(models.Model):
     class Meta:
         verbose_name = 'Пост'
         verbose_name_plural = 'Посты'
-
-
